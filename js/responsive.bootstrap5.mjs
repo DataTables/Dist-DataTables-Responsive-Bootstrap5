dc5c5db5f6f3a8800615b4241b8b59b002d344c6
dtbuild <dtbuild@rigil>
2026-05-07 13:50:52 +0000

    4fcbb8f4f2f1f9631a60e637c9d120378d5aaa3c
    Sync to source repo @2233e6926472f98a719e9d28464f49c54f4680df

--- js/responsive.bootstrap5.mjs
+++ js/responsive.bootstrap5.mjs
@@ -1,29 +1,46 @@
-/*! Bootstrap 5 integration for DataTables' Responsive
- * © SpryMedia Ltd - datatables.net/license
+/*! Responsive Bootstrap 5 styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
-import jQuery from 'jquery';
 import DataTable from 'datatables.net-bs5';
 import Responsive from 'datatables.net-responsive';
 
-// Allow reassignment of the $ variable
-let $ = jQuery;
 
-
+var Dom = DataTable.Dom;
 var _display = DataTable.Responsive.display;
 var _original = _display.modal;
-var _modal = $(
-	'<div class="modal fade dtr-bs-modal" role="dialog">' +
-		'<div class="modal-dialog" role="document">' +
-		'<div class="modal-content">' +
-		'<div class="modal-header">' +
-		'<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
-		'</div>' +
-		'<div class="modal-body"/>' +
-		'</div>' +
-		'</div>' +
-		'</div>'
-);
+
+var _modal = Dom
+	.c('div')
+	.classAdd('modal fade dtr-bs-modal')
+	.attr('role', 'dialog')
+	.append(
+		Dom
+			.c('div')
+			.classAdd('modal-dialog')
+			.attr('role', 'document')
+			.append(
+				Dom
+					.c('div')
+					.classAdd('modal-content')
+					.append(
+						Dom
+							.c('div')
+							.classAdd('modal-header')
+							.append(
+								Dom
+									.c('button')
+									.attr('type', 'button')
+									.attr('data-bs-dismiss', 'modal')
+									.attr('aria-label', 'Close')
+									.classAdd('btn-close')
+							)
+					)
+					.append(Dom.c('div').classAdd('modal-body'))
+			)
+	)
+	.append(Dom.c('div').classAdd('content'));
+
 var modal;
 
 // Note this could be undefined at the time of initialisation - the
@@ -47,17 +64,19 @@ function getBs() {
 		return _bs;
 	}
 
-	throw new Error('No Bootstrap library. Set it with `DataTable.use(bootstrap);`');
+	throw new Error(
+		'No Bootstrap library. Set it with `DataTable.use(bootstrap);`'
+	);
 }
 
 _display.modal = function (options) {
 	if (!modal && _bs.Modal) {
 		let localBs = getBs();
-		modal = new localBs.Modal(_modal[0]);
+		modal = new localBs.Modal(_modal.get(0));
 	}
 
 	return function (row, update, render, closeCallback) {
-		if (! modal) {
+		if (!modal) {
 			return _original(row, update, render, closeCallback);
 		}
 		else {
@@ -74,7 +93,12 @@ _display.modal = function (options) {
 
 					header
 						.empty()
-						.append('<h4 class="modal-title">' + options.header(row) + '</h4>')
+						.append(
+							Dom
+								.c('h4')
+								.classAdd('modal-title')
+								.html(options.header(row))
+						)
 						.append(button);
 				}
 
@@ -82,13 +106,19 @@ _display.modal = function (options) {
 
 				_modal
 					.data('dtr-row-idx', row.index())
-					.one('hidden.bs.modal', closeCallback)
 					.appendTo('body');
+
+				_modal.get(0).addEventListener('hidden.bs.modal', closeCallback, {
+					once: true
+				});
 
 				modal.show();
 			}
 			else {
-				if ($.contains(document, _modal[0]) && row.index() === _modal.data('dtr-row-idx')) {
+				if (
+					_modal.isAttached() &&
+					row.index() === _modal.data('dtr-row-idx')
+				) {
 					_modal.find('div.modal-body').empty().append(rendered);
 				}
 				else {
@@ -104,3 +134,4 @@ _display.modal = function (options) {
 
 
 export default DataTable;
+
