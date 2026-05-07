072a31ef7aa8fa19776004b2dbda8b1d9e3a984c
Allan Jardine <allan@datatables.net>
2026-05-07 15:36:49 +0000

    Updating package for DataTables 3

--- js/responsive.bootstrap5.js
+++ js/responsive.bootstrap5.js
@@ -1,21 +1,87 @@
-/*! Bootstrap 5 integration for DataTables' Responsive
- * © SpryMedia Ltd - datatables.net/license
+/*! Responsive Bootstrap 5 styling 4.0.0-beta.1 for DataTables
+ * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
+(function(factory){
+	if (typeof define === 'function' && define.amd) {
+		// AMD
+		define(['datatables.net-bs5', 'datatables.net-responsive'], function (dt) {
+			return factory(window, document, dt);
+		});
+	}
+	else if (typeof exports === 'object') {
+		// CommonJS
+		var cjsRequires = function (root) {
+			if (! root.DataTable) {
+				require('datatables.net-bs5')(root);
+			}
+
+			if (! window.DataTable.Responsive) {
+				require('datatables.net-responsive')(root);
+			}
+		};
+
+		if (typeof window === 'undefined') {
+			module.exports = function (root) {
+				if (! root) {
+					// CommonJS environments without a window global must pass a
+					// root. This will give an error otherwise
+					root = window;
+				}
+
+				cjsRequires(root);
+				return factory(root, root.document, root.DataTable);
+			};
+		}
+		else {
+			cjsRequires(window);
+			module.exports = factory(window, window.document, window.DataTable);
+		}
+	}
+	else {
+		// Browser
+		factory(window, document, window.DataTable);
+	}
+}(function(window, document, DataTable) {
+'use strict';
+
+
+
+var Dom = DataTable.Dom;
 var _display = DataTable.Responsive.display;
 var _original = _display.modal;
-var _modal = $(
-	'<div class="modal fade dtr-bs-modal" role="dialog">' +
-		'<div class="modal-dialog" role="document">' +
-		'<div class="modal-content">' +
-		'<div class="modal-header">' +
-		'<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
-		'</div>' +
-		'<div class="modal-body"/>' +
-		'</div>' +
-		'</div>' +
-		'</div>'
-);
+
+var _modal = Dom
+	.c('div')
+	.classAdd('modal fade dtr-bs-modal')
+	.attr('role', 'dialog')
+	.append(
+		Dom
+			.c('div')
+			.classAdd('modal-dialog')
+			.attr('role', 'document')
+			.append(
+				Dom
+					.c('div')
+					.classAdd('modal-content')
+					.append(
+						Dom
+							.c('div')
+							.classAdd('modal-header')
+							.append(
+								Dom
+									.c('button')
+									.attr('type', 'button')
+									.attr('data-bs-dismiss', 'modal')
+									.attr('aria-label', 'Close')
+									.classAdd('btn-close')
+							)
+					)
+					.append(Dom.c('div').classAdd('modal-body'))
+			)
+	)
+	.append(Dom.c('div').classAdd('content'));
+
 var modal;
 
 // Note this could be undefined at the time of initialisation - the
@@ -39,17 +105,19 @@ function getBs() {
 		return _bs;
 	}
 
-	throw new Error('No Bootstrap library. Set it with `DataTable.use(bootstrap);`');
+	throw new Error(
+		'No Bootstrap library. Set it with `DataTable.use(bootstrap);`'
+	);
 }
 
 _display.modal = function (options) {
 	if (!modal && _bs.Modal) {
 		let localBs = getBs();
-		modal = new localBs.Modal(_modal[0]);
+		modal = new localBs.Modal(_modal.get(0));
 	}
 
 	return function (row, update, render, closeCallback) {
-		if (! modal) {
+		if (!modal) {
 			return _original(row, update, render, closeCallback);
 		}
 		else {
@@ -66,7 +134,12 @@ _display.modal = function (options) {
 
 					header
 						.empty()
-						.append('<h4 class="modal-title">' + options.header(row) + '</h4>')
+						.append(
+							Dom
+								.c('h4')
+								.classAdd('modal-title')
+								.html(options.header(row))
+						)
 						.append(button);
 				}
 
@@ -74,13 +147,19 @@ _display.modal = function (options) {
 
 				_modal
 					.data('dtr-row-idx', row.index())
-					.one('hidden.bs.modal', closeCallback)
 					.appendTo('body');
+
+				_modal.get(0).addEventListener('hidden.bs.modal', closeCallback, {
+					once: true
+				});
 
 				modal.show();
 			}
 			else {
-				if ($.contains(document, _modal[0]) && row.index() === _modal.data('dtr-row-idx')) {
+				if (
+					_modal.isAttached() &&
+					row.index() === _modal.data('dtr-row-idx')
+				) {
 					_modal.find('div.modal-body').empty().append(rendered);
 				}
 				else {
@@ -93,3 +172,7 @@ _display.modal = function (options) {
 		}
 	};
 };
+
+
+return DataTable;
+}));
